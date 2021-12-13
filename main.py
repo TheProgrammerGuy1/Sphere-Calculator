ad329d4159f487d08ff321a192abe95ec3fd0785
TheProgrammerGuy1 <89099150+TheProgrammerGuy1@users.noreply.github.com>
2021-12-13 13:29:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,151 @@
+#Program to calculate Diameter,Circumference,Area of circles and semi-circles
+#Welcome
+print("Welcome to Sphere Calculator")
+#Taking input from the user
+kind = input("Of whose calculation you want to do (circle,semi-circle): ")
+radius = input("Radius: ")
+reason = input("What you want to calculate (Diameter,Circumference,Area): ")
+#Conversion of radius form string to integer
+radius = int(radius)
+#Calculation of diameter of circle
+if reason == "diameter" and kind == "circle":
+    print("The diameter of circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "Circle":
+    print("The diameter of circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "circle":
+    print("The diameter of circle: "+str(radius*2))
+elif reason == "diameter" and kind == "Circle":
+    print("The diameter of circle: "+str(radius*2))
+
+#Calculation of diameter of semi-circle
+elif reason == "diameter" and kind == "semi circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "semi circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "diameter" and kind == "Semi circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "Semi circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "diameter" and kind == "semi Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "semi Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "diameter" and kind == "Semi Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "Semi Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+
+#Calculation of diameter of semi-circle (with hyphen)
+elif reason == "diameter" and kind == "semi-circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "semi-circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "diameter" and kind == "Semi-circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "Semi-circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "diameter" and kind == "semi-Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "semi-Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "diameter" and kind == "Semi-Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+elif reason == "Diameter" and kind == "Semi-Circle":
+    print("The diameter of semi-circle: "+str(radius*2))
+
+#Calculation of circumference of circle
+elif reason == "circumference" and kind == "circle":
+    print("The circumference of circle: "+str(2*22/7*radius))
+elif reason == "Circumference" and kind == "Circle":
+    print("The circumference of circle: "+str(2*22/7*radius))
+elif reason == "Circumference" and kind == "circle":
+    print("The circumference of circle: "+str(2*22/7*radius))
+elif reason == "circumference" and kind == "Circle":
+    print("The circumference of circle: "+str(2*22/7*radius))
+
+#Calculation of circumference of semi-circle
+elif reason == "circumference" and kind == "semi circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "semi circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "circumference" and kind == "Semi circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "Semi circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "circumference" and kind == "semi Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "semi Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "circumference" and kind == "Semi Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "Semi Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+
+#Calculation of circumference of semi-circle (with hyphen)
+elif reason == "circumference" and kind == "semi-circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "semi-circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "circumference" and kind == "Semi-circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "Semi-circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "circumference" and kind == "semi-Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "semi-Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "circumference" and kind == "Semi-Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+elif reason == "Circumference" and kind == "Semi-Circle":
+    print("The circumference of semi-cirle: "+str((22/7*radius)+(2*radius)))
+
+#Calculation of area of circle:
+elif reason == "area" and kind == "circle":
+    print("The area of circle: "+str(22/7*radius**2))
+elif reason == "Area" and kind == "Circle":
+    print("The area of circle: "+str(22/7*radius**2))
+elif reason == "Area" and kind == "circle":
+    print("The area of circle: "+str(22/7*radius**2))
+elif reason == "area" and kind == "Circle":
+    print("The area of circle: "+str(22/7*radius**2))
+
+#Calculation of area of semi-circle
+elif reason == "area" and kind == "semi circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "semi circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "area" and kind == "Semi circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "Semi circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "area" and kind == "semi Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "semi Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "area" and kind == "Semi Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "Semi Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+
+#Calculation of area of semi-circle (with hyphen)
+elif reason == "area" and kind == "semi-circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "semi-circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "area" and kind == "Semi-circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "Semi-circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "area" and kind == "semi-Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "semi-Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "area" and kind == "Semi-Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+elif reason == "Area" and kind == "Semi-Circle":
+    print("The area of semi-circle: "+str(1/2*(22/7*radius**2)))
+#For wrong inputs
+else:
+    print("Invalid Values")
+#Wishing thanks and the end
+print("Thanks for using our tool")
